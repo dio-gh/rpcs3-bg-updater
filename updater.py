2815848ae635338701584798710582e849598dee
dio <10439488+dio-gh@users.noreply.github.com>
2022-03-30 19:44:40 +0200

    some tightening

--- updater.py
+++ updater.py
@@ -1,25 +1,23 @@
-import requests
-import subprocess
-import re
-import os
+import requests as req
+import subprocess, re, os
 
 LOCAL_COMMIT = '4a86638c'
 SHORTHASH_PATTERN = r'[0-9a-fA-F]{8}'
 
 API_ENDPOINT = 'https://update.rpcs3.net'
-API_VERSION_ID = 'v2'
-
-os.chdir(os.path.dirname(__file__))
+API_VER = 'v2'
 
 # example url: https://update.rpcs3.net/?api=v2&c=4a86638c
-api_response = requests.get(API_ENDPOINT, params={'api': API_VERSION_ID, 'c': LOCAL_COMMIT}).json()
+res = req.get(API_ENDPOINT, params={'api': API_VER, 'c': LOCAL_COMMIT}).json()
 
-# when the return_code is 1, the latest update shall be applied
-if api_response["return_code"] == 1:
+# when the return_code is 1, gotta update
+if res["return_code"] == 1:
+    os.chdir(os.path.dirname(__file__))    # ensure correct working dir
 
     # retrieve the update archive
-    update_blob = requests.get(api_response["latest_build"]["windows"]["download"])
-    update_fname = re.findall('filename=(.+)', update_blob.headers.get('content-disposition'))[0]
+    update_blob = req.get(res["latest_build"]["windows"]["download"])
+    update_meta = update_blob.headers.get('content-disposition')
+    update_fname = re.findall('filename=(.+)', update_meta)[0]
     open(update_fname, 'wb').write(update_blob.content)
 
     # extract the update archive
@@ -34,7 +32,7 @@ if api_response["return_code"] == 1:
     # erase the update archive
     os.remove(update_fname)
 
-    # update the local commit hash using the just retrieved update's filename if all went well
+    # on success, update the local commit hash based on the update's filename
     if extraction_status == 0:
         with open(os.path.basename(__file__), 'r+') as this_script:
             updated_script = re.sub(
